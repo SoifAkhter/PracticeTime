f83363c8f88c01be209cc9d6182945397026deeb
Soif Akhter <soifakhter@gmail.com>
2020-03-03 18:48:49 +0530

    Correction in map function

--- merge_sort.py
+++ merge_sort.py
@@ -25,5 +25,5 @@ def merge_sort(X,left,right):
 		R = merge_sort(X,mid,right)
 		return merge(L,R)
 
-lst = list(map(int,input("Enter List: ")).split()))
+lst = list(map(int,input("Enter List: ").split()))
 print("Sorted List: ",merge_sort(lst,0,len(lst)))
