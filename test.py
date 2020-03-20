500528c91f0438545dbb258e9d9de3b0fb494933
Soif Akhter <soifakhter@gmail.com>
2020-03-20 23:56:57 +0530

    Testing

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-lst = [43,545,67,7,8,999,45,34]
-lst[2:5] += [100]
-print(lst)
+def maxSum(n, m, arr):
+    buff = n//m
+    r =n%m
+    gr = []
+    b = r*(buff+1)
+    for i in range(0,b,buff+1):
+        gr.append(arr[i:i+buff+1])
+    for i in range(b,n, buff):
+        gr.append(arr[i:i+buff])
+    print(gr) 
+
+arr = [300,405,506,21,45,765,2352,65]
+arr.sort()
+print(arr)
