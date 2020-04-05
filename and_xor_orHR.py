59591ec61b761fbd35f326618febc3b4dd1c9e14
Soif Akhter <soifakhter@gmail.com>
2020-04-05 23:26:04 +0530

    Hacker Rank Problem

---
+++ and_xor_orHR.py
@@ -0,0 +1,19 @@
+def andXorOr(a):
+    maxx = a[0]+ a[1]
+    m1 = a[0]
+    m2 = a[1]
+    for i in range(1,len(a)-1):
+    	if m2 == a[i+1]:
+    		continue
+    	elif maxx < m1+a[i+1]:
+            m1 = m2
+            m2 = a[i+1]
+            maxx = m1+m2
+    print("Max: {} , m1: {}, m2: {}".format(maxx,m1,m2))
+    return (((m1 & m2) ^ (m1 | m2)) & (m1 ^ m2))
+print("Give Input: ")
+a_count = int(input())
+
+a = list(map(int, input().rstrip().split()))    
+result = andXorOr(a)
+print("result is: ",result)
