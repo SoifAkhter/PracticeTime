21cac9d7b5f7cd34c6cb6780d5b8d7586de948ba
Soif Akhter <soifakhter@gmail.com>
2020-04-17 23:20:57 +0530

    No of divisor of a Number - HackerEarth Problem

---
+++ number_divisible.py
@@ -0,0 +1,16 @@
+# Write your code here
+def findDiv(n):
+    count = 2
+    if n == 1:
+        return 1
+    elif n == 2 or n == 3:
+        return 2
+    else:
+        for i in range(2,n//2 + 1):
+            if n % i == 0:
+                count +=1
+    return count
+
+t = int(input())
+for _ in range(t):
+    print(findDiv(int(input())))
